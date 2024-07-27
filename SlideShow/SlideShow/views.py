f999d8e504fc9b974100500447136d99c445cc38
arminmahdavian <armin.mail007@gmail.com>
2024-07-27 17:07:57 +0100

    initial commit

---
+++ SlideShow/SlideShow/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import logout
+
+
+# Create your views here.
+
+
+def home(request):
+    return render(request, 'SlideShow/home.html')
+
+
+def logout_view(request):
+    logout(request)
+    return redirect(request.GET.get('next'))
+
